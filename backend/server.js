96f674b7572a3f1c43ff2bfd08817fcce7068c6e
Kyle <kyle.treoxer@gmail.com>
2022-06-18 12:56:04 +0700

    accessed twitter

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
-const Twitter = require('twitter');
+require ('dotenv').config();
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+var params = {screen_name: 'nodejs'};
+
+client.get('users/show', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  } else {
+    console.log(error);
+  }
+});
 
 app.get("/api", (req,res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
